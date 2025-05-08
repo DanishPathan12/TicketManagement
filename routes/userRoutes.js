1600c9cf6d660bc82229927cc7a75934fef797ce
Danish Pathan <danishpathan7477@gmail.com>
2025-05-08 16:13:01 +0530

    remove cloudinary

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const storage = require("../utils/storage.js");
 const { Register, Login, } = require("../controllers/user.controller.js")
 
 
 const upload = multer({ storage });
 
-router.post("/register", upload.single("photo"), Register);
+router.post("/register", Register);
 
 router.post("/login", Login);
 
